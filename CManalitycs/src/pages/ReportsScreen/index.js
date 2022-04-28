e61963cc6e201406f5e1541eb75789e507e13f15
david cabral <david1001cabral@hotmail.com>
2022-04-28 01:32:11 -0300

    chart nao funcionando

--- CManalitycs/src/pages/ReportsScreen/index.js
+++ CManalitycs/src/pages/ReportsScreen/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
-// import {} from 'react-native';
-import {Container, Title} from './styles';
+
+import { Chart } from "react-google-charts";
+
+import {Container, Header, Title} from './styles';
 
 const ReportsScreen = () => {
+
+  const data = [
+    ["Year", "Sales", "Expenses", "Profit"],
+    ["2014", 1000, 400, 200],
+    ["2015", 1170, 460, 250],
+    ["2016", 660, 1120, 300],
+    ["2017", 1030, 540, 350],
+  ];
+  
+  const options = {
+    chart: {
+      title: "Company Performance",
+      subtitle: "Sales, Expenses, and Profit: 2014-2017",
+    },
+  };
+
   return (
     <Container>
-      <Title> Dalhe </Title>
+      <Header>
+        <Title> Resumo da Escola </Title>
+      </Header>
+
+      <Chart
+      chartType="Bar"
+      width="100%"
+      height="400px"
+      data={data}
+      options={options}
+    />
+      
     </Container>
   );
 };
--- CManalitycs/src/pages/ReportsScreen/styles.js
+++ CManalitycs/src/pages/ReportsScreen/styles.js
@@ -3,12 +3,21 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   align-items: center;
-  justify-content: space-between;
+  justify-content: flex-start;
   background-color: #fff;
 `;
-export const Title = styled.Text`
-  flex: 1;
+
+export const Header = styled.View`
+  width: 100%;
   align-items: center;
-  justify-content: space-between;
-  background-color: #fff;
+  justify-content: center;
+  background-color: #bfbfbf;
+  height: 12%;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+`;
+
+export const Title = styled.Text`
+  font-size: 17px;
+  font-weight: 600;
 `;
